363d73e801054d84765147374c673bac77cf612f
Eduardo Kraus - Aulas <eduardokraus@MacNovo-do-kraus.local>
2017-09-24 05:41:52 -0300

    Initial commit

---
+++ settings.php
@@ -0,0 +1,99 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   theme_boosta
+ * @copyright 2016 Ryan Wyllie
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+    $settings = new theme_boosta_admin_settingspage_tabs('themesettingboosta', get_string('configtitle', 'theme_boosta'));
+    $page = new admin_settingpage('theme_boosta_general', get_string('generalsettings', 'theme_boosta'));
+
+    // Preset.
+    $name = 'theme_boosta/preset';
+    $title = get_string('preset', 'theme_boosta');
+    $description = get_string('preset_desc', 'theme_boosta');
+    $default = 'blue';
+
+    $choices = [];
+    $choices['blue'] = 'blue';
+    $choices['green'] = 'green';
+    $choices['black'] = 'black';
+    $choices['red'] = 'red';
+    $choices['yellow'] = 'yellow';
+    $choices['yellow2'] = 'yellow2';
+
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Image Background.
+    $name = 'theme_boosta/background';
+    $title = get_string('background', 'theme_boosta');
+    $description = get_string('background_desc', 'theme_boosta');
+    $default = 'math-write';
+
+    $choices = [];
+    $choices['math-black'] = 'math-black';
+    $choices['math-write'] = 'math-write';
+    $choices['students-black'] = 'students-black';
+    $choices['users-black'] = 'users-black';
+    $choices['users-write'] = 'users-write';
+
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Variable $body-color.
+    // We use an empty default value because the default colour should come from the preset.
+//    $name = 'theme_boosta/brandcolor';
+//    $title = get_string('brandcolor', 'theme_boosta');
+//    $description = get_string('brandcolor_desc', 'theme_boosta');
+//    $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
+//    $setting->set_updatedcallback('theme_reset_all_caches');
+//    $page->add($setting);
+
+
+    // Must add the page after definiting all the settings!
+    $settings->add($page);
+
+    // Advanced settings.
+    $page = new admin_settingpage('theme_boosta_advanced', get_string('advancedsettings', 'theme_boosta'));
+
+    // Raw SCSS to include before the content.
+    $setting = new admin_setting_scsscode('theme_boosta/scsspre',
+        get_string('rawscsspre', 'theme_boosta'), get_string('rawscsspre_desc', 'theme_boosta'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Raw SCSS to include after the content.
+    $setting = new admin_setting_scsscode('theme_boosta/scss', get_string('rawscss', 'theme_boosta'),
+        get_string('rawscss_desc', 'theme_boosta'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $settings->add($page);
+}
+
+$s_theme_boosta_preset = optional_param('s_theme_boosta_preset', false, PARAM_TEXT);
+if ($s_theme_boosta_preset)  {
+    theme_reset_all_caches();
+    header("Location: {$CFG->wwwroot}/admin/settings.php?section=themesettingboosta&key=".time());
+}
