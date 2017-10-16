695c890538fcc165b0630213b52835177a57d651
Eduardo Kraus - Aulas <eduardokraus@MacNovo-do-kraus.local>
2017-10-16 15:43:29 -0200

    Renomeando de BoostA para Boost Magnific

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017101602;
+$plugin->version   = 2017101603;
 $plugin->requires  = 2016120505;
-$plugin->release = 'V1.6';
+$plugin->release = 'V2.2';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'theme_boost_magnific';
