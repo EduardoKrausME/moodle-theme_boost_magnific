15b2ddb78b66740576cdca81a56dae9612bb8c1d
Eduardo Kraus <kraus@eduardokraus.com>
2018-01-16 19:18:22 -0200

    Bug Color

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017111700;
+$plugin->version = 2018011600;
 $plugin->requires = 2016120500;
 $plugin->release = 'V2.8';
 $plugin->maturity = MATURITY_STABLE;
