889b1142ca41267c05fe205e0be2b8c63d4fde76
Eduardo Kraus <kraus@MacNovo-do-kraus.local>
2018-02-22 23:09:41 -0300

    #3

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018012200;
+$plugin->version = 2018022200;
 $plugin->requires = 2016120500;
-$plugin->release = 'V2.9.1';
+$plugin->release = 'V2.9.2';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'theme_boost_magnific';
 
