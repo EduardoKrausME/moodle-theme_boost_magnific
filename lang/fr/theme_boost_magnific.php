856127dea2a35b4218b42e2c9c826acd8e232b17
Eduardo Kraus <kraus@eduardokraus.com>
2024-03-07 14:22:49 -0300

    fr

---
+++ lang/fr/theme_boost_magnific.php
@@ -0,0 +1,122 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_boost_magnific', language 'fr'
+ *
+ * @package     theme_boost_magnific
+ * @copyright   2024 Eduardo kraus (http://eduardokraus.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'boost_magnific';
+$string['choosereadme'] = 'boost_magnific est un thème conçu avec soin pour apporter des couleurs vives à Moodle.';
+
+// Slides.
+$string['settings_slideshow_heading'] = 'Diaporama';
+$string['theme_boost_magnific_slideshow_editbooton'] = 'Modifier le diaporama';
+
+$string['slideshow_numslides'] = 'Nombre d\'images dans le diaporama';
+$string['slideshow_numslides_desc'] = 'Combien d\'images souhaitez-vous dans le diaporama ?';
+$string['slideshow_numslides_nenhum'] = 'Aucune diapositive sur la page d\'accueil';
+
+$string['slideshow_info'] = 'Diapositive {$a}';
+
+$string['slidecaption_desc'] = 'Entrez le texte de la légende à utiliser sur la diapositive';
+$string['slideshow_image'] = 'Image de la diapositive';
+$string['slideshow_image_desc'] = 'L\'image doit avoir 1250px X 400px.';
+
+$string['slideshow_url'] = 'Lien du bouton des diapositives';
+$string['slideshow_url_desc'] = 'Insérez la destination du lien du bouton de l\'image de la diapositive';
+
+$string['slideshow_text'] = 'Texte court descriptif de la diapositive';
+$string['slideshow_text_desc'] = 'Insérez un petit texte sur la diapositive.';
+
+
+// À propos.
+$string['settings_about_heading'] = 'À propos de votre Moodle';
+$string['theme_boost_magnific_about_editbooton'] = 'Modifier le bloc À propos';
+$string['frontpage_about_enable'] = 'Activer le bloc À propos';
+$string['frontpage_about_enable_desc'] = 'Si coché, le bloc À propos apparaîtra sous la bannière !';
+$string['frontpage_about_title'] = 'Titre du bloc À propos';
+$string['frontpage_about_title_default'] = 'Notre communauté mondiale';
+$string['frontpage_about_logo'] = 'Logo différent à afficher ici';
+$string['frontpage_about_logo_desc'] = 'Si défini, ce logo sera utilisé ici au lieu du logo en haut.<br> Blanc utilise le logo en haut !';
+$string['frontpage_about_description'] = 'Décrivez ce que vous faites';
+$string['frontpage_about_description_desc'] = 'Décrivez en maximum 5 lignes la finalité de votre Moodle';
+$string['frontpage_about_info'] = 'Boîte de données {$a}';
+$string['frontpage_about_text'] = 'Nom de la donnée';
+$string['frontpage_about_text_desc'] = 'Placez ici le nom de la donnée qui sera affichée sur la page d\'accueil';
+$string['frontpage_about_number'] = 'Quantité de données';
+$string['frontpage_about_number_desc'] = 'Placez ici la quantité d\'informations mentionnée ci-dessus';
+$string['frontpage_about_text_1_defalt'] = 'Cours';
+$string['frontpage_about_text_2_defalt'] = 'Enseignants';
+$string['frontpage_about_text_3_defalt'] = 'Étudiants';
+$string['frontpage_about_text_4_defalt'] = 'Leçons';
+
+
+$string['theme_boost_magnific_frontpage_home'] = 'Blocs de la page d\'accueil';
+$string['heart'] = 'Si vous aimez ce thème, n\'oubliez pas de cliquer sur ❤️ sur la page des thèmes <a href="{$a}" target="_blank">en cliquant ici</a>';
+$string['theme_boost_magnific_frontpage_bloco'] = 'Bloc "{$a}"';
+
+$string['footer_frontpage_blockcourses_text'] = 'Texte court expliquant le bloc "{$a}"';
+$string['footer_frontpage_blockcourses_text_desc'] = 'Ajoutez un texte parlant des "{$a}" !';
+$string['footer_frontpage_blockcourses_instructor'] = 'Afficher le nom du professeur';
+$string['footer_frontpage_blockcourses_instructor_desc'] = 'Si coché, affiche les noms des professeurs dans la liste des cours !';
+$string['instructor'] = 'Professeur';
+$string['acessar'] = 'Accéder au cours';
+$string['continuar'] = 'Continuer à étudier';
+$string['matricular'] = 'S\'inscrire';
+$string['countlesson'] = '{$a} leçon';
+$string['countlessons'] = '{$a} leçons';
+$string['free_name'] = 'Gratuit';
+
+$string['favicon'] = 'Favicon';
+$string['favicon_desc'] = 'Le favicon est affiché à côté du titre de la page dans l\'onglet du navigateur. Un favicon Moodle est affiché si un favicon personnalisé n\'est pas fourni.';
+
+
+// Pied de page.
+$string['social_twitter'] = 'Votre Twitter';
+$string['social_twitter_desc'] = 'L\'URL Twitter de votre organisation.';
+$string['social_youtube'] = 'Votre Youtube';
+$string['social_youtube_desc'] = 'L\'URL YouTube de votre organisation.';
+$string['social_linkedin'] = 'Votre Linkedin';
+$string['social_linkedin_desc'] = 'L\'URL Linkedin de votre organisation.';
+$string['social_instagram'] = 'Votre Instagram';
+$string['social_instagram_desc'] = 'L\'URL Instagram de votre organisation.';
+$string['social_facebook'] = 'Votre Facebook';
+$string['social_facebook_desc'] = 'L\'URL Facebook de votre organisation.';
+
+
+$string['settings_topo_heading'] = 'Haut';
+
+$string['settings_footer_heading'] = 'Bloc du bas de page';
+
+$string['footerblock_description'] = 'Bloc de description';
+$string['footer_description'] = 'Description';
+$string['footer_description_desc'] = 'Décrivez votre Moodle, ce que vous faites, et cette information sera affichée sous le logo dans le bas de page de Moodle';
+
+$string['footerblock_links'] = 'Bloc des liens';
+$string['footer_links_title'] = 'Titre du bloc des liens';
+$string['footer_links_title_default'] = 'Liens importants';
+
+$string['footerblock_social'] = 'Bloc social';
+$string['footer_social_title'] = 'Titre du bloc des réseaux sociaux';
+$string['footer_social_title_desc'] = 'Placez le titre du bloc qui apparaîtra dans le bas de page avec les données de vos réseaux sociaux.';
+$string['footer_social_title_default'] = 'Suivez-nous sur les réseaux sociaux';
+$string['footerblock_contact'] = 'Bloc de contact';
+
+$string['footerblink'] = 'Liens du bloc du bas de page';
