da3dbf6973e2a3a61a169cc78d0204b991342108
Eduardo Kraus <kraus@eduardokraus.com>
2024-03-07 13:19:47 -0300

    V 5.0.2

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024030700;
+$plugin->version = 2024030702;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '5.0.1';
+$plugin->release = '5.0.2';
 $plugin->requires = 2022041900;
 $plugin->component = 'theme_boost_magnific';
 $plugin->dependencies = array(
