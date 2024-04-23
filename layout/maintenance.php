b2a41b083c654bbe215dc92cb3f5c711a7f31634
Eduardo Kraus <kraus@eduardokraus.com>
2024-04-22 21:33:22 -0300

    Código

--- layout/maintenance.php
+++ layout/maintenance.php
@@ -22,7 +22,7 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+defined('MOODLE_INTERNAL') || die;
 
 $logourl = theme_boost_magnific_get_logo("header");
 $data = [
