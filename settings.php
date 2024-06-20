f8c569d57d9a0aaf98a33f5f69fdffe784a36d02
Eduardo Kraus <kraus@eduardokraus.com>
2024-06-20 19:33:30 -0300

    Na lista de cursos muda a ordem das infos pra cima

--- settings.php
+++ settings.php
@@ -42,6 +42,8 @@ if ($ADMIN->fulltree) {
 
     require_once(__DIR__ . "/settings-slideshow.php");
 
+    require_once(__DIR__ . "/settings-mycourses.php");
+
     if (get_config('theme_boost_magnific', 'home_type') == 0) {
         require_once(__DIR__ . "/settings-about.php");
     }
