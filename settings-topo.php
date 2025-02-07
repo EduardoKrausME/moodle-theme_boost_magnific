86f42831b54df318fd61569098ae2ca3f212410b
Eduardo Kraus <kraus@eduardokraus.com>
2025-02-07 12:30:41 -0300

    Novo Topo

--- settings-topo.php
+++ settings-topo.php
@@ -17,17 +17,28 @@
 /**
  * Theme Settings File
  *
- * @package     theme_boost_magnific
- * @copyright   2024 Eduardo Kraus https://eduardokraus.com/
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   theme_boost_magnific
+ * @copyright 2024 Eduardo Kraus https://eduardokraus.com/
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$page = new admin_settingpage('theme_boost_magnific_topo', get_string('settings_top_heading', 'theme_boost_magnific'));
+$page = new admin_settingpage("theme_boost_magnific_top", get_string("settings_top_heading", "theme_boost_magnific"));
 
-$page->add(new admin_setting_configtextarea('custommenuitems',
-    get_string('custommenuitems', 'theme_boost_magnific'),
-    get_string('custommenuitems_desc', 'theme_boost_magnific'), '', PARAM_RAW, '50', '10'));
+// Profile background image.
+$setting = new admin_setting_configstoredfile("theme_boost_magnific/background_profile_image",
+    get_string("background_profile_image", "theme_boost_magnific"),
+    get_string("background_profile_image_desc", "theme_boost_magnific"),
+    "background_profile_image", 0,
+    ["maxfiles" => 1, "accepted_types" => [".jpg", ".jpeg", ".svg", ".png"]]);
+$setting->set_updatedcallback("theme_reset_all_caches");
+$page->add($setting);
+
+$setting = new admin_setting_configtextarea("custommenuitems",
+    get_string("custommenuitems", "theme_boost_magnific"),
+    get_string("custommenuitems_desc", "theme_boost_magnific"),
+    "", PARAM_RAW, "50", "10");
+$page->add($setting);
 
 $settings->add($page);
