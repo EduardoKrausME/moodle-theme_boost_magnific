7b0651d29b87ae9057dcb2191d19a6276a9fce0f
Eduardo Kraus <kraus@eduardokraus.com>
2025-02-07 12:18:32 -0300

    Spaces

--- _editor/index.php
+++ _editor/index.php
@@ -17,9 +17,9 @@
 /**
  * Editor.
  *
- * @package     theme_boost_magnific
- * @copyright   2024 Eduardo kraus (http://eduardokraus.com)
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   theme_boost_magnific
+ * @copyright 2024 Eduardo kraus (http://eduardokraus.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once('../../../config.php');
