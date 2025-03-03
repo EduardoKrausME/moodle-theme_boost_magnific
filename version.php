847fdff16702a74e4c8905c623bb96bb6b4663e8
Eduardo Kraus <kraus@eduardokraus.com>
2025-03-03 16:23:45 -0300

    V 8.3.7

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2025030200;
-$plugin->release = "8.3.6";
+$plugin->version = 2025030300;
+$plugin->release = "8.3.7";
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022041900;
 $plugin->component = "theme_boost_magnific";
