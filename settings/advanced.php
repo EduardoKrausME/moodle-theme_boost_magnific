0f2915196b07de852a62476e6de228fdb1225dc7
Eduardo Kraus <kraus@eduardokraus.com>
2025-10-12 18:10:15 -0300

    validates a SCSS snippet entered in a textarea

--- settings/advanced.php
+++ settings/advanced.php
@@ -22,23 +22,30 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use theme_boost_magnific\admin\setting_scss;
+
 defined('MOODLE_INTERNAL') || die;
 
 // Advanced settings.
 $page = new admin_settingpage("theme_boost_magnific_advanced", get_string("advancedsettings", "theme_boost_magnific"));
 
 // Raw SCSS to include before the content.
-$setting = new admin_setting_scsscode('theme_boost_magnific/scsspre',
-    get_string('rawscsspre', 'theme_boost'),
-    get_string('rawscsspre_desc', 'theme_boost'), '', PARAM_RAW);
-$setting->set_updatedcallback('theme_reset_all_caches');
+$setting = new setting_scss(
+    "theme_boost_magnific/scsspre",
+    get_string("rawscsspre", "theme_boost"),
+    get_string("rawscsspre_desc", "theme_boost"),
+    "", PARAM_RAW
+);
+$setting->set_updatedcallback("theme_reset_all_caches");
 $page->add($setting);
 
 // Raw SCSS to include after the content.
-$setting = new admin_setting_scsscode('theme_boost_magnific/scsspos',
-    get_string('rawscss', 'theme_boost'),
-    get_string('rawscss_desc', 'theme_boost'), '', PARAM_RAW);
-$setting->set_updatedcallback('theme_reset_all_caches');
+$setting = new setting_scss(
+    "theme_boost_magnific/scsspos", get_string("rawscss", "theme_boost"),
+    get_string("rawscss_desc", "theme_boost"),
+    "", PARAM_RAW
+);
+$setting->set_updatedcallback("theme_reset_all_caches");
 $page->add($setting);
 
 $settings->add($page);
