2fd0d32a7461eb95b35dd0221e4ffc5c60cf945f
Eduardo Kraus <kraus@eduardokraus.com>
2026-01-25 09:27:27 -0300

    PHP8+

--- db/hooks.php
+++ db/hooks.php
@@ -34,6 +34,6 @@ $callbacks = [
     ],
     [
         "hook" => before_footer_html_generation::class,
-        "callback" => 'theme_boost_magnific\core_hook_output::before_footer_html_generation',
+        "callback" => '\theme_boost_magnific\core_hook_output::before_footer_html_generation',
     ],
 ];
