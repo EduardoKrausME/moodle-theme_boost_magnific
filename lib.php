92bdb914c7e2b7524e6813ed1bbacb54d60a78f7
Eduardo Kraus <kraus@eduardokraus.com>
2026-02-27 17:14:46 -0300

    Institutional layout $navbar-height

--- lib.php
+++ lib.php
@@ -205,6 +205,10 @@ function theme_boost_magnific_get_pre_scss($theme) {
         \$footer-bg    : {$footerbg};
         \$footer-color : {$footercolor};\n";
 
+    if (get_config("theme_eadtraining", "navbarlayout") === "institutional") {
+        $scss .= "\$navbar-height : 127px;\n";
+    }
+
     if ($CFG->theme == "degrade") {
         $angle = theme_boost_magnific_default("angle", 30);
         $gradient1 = theme_boost_magnific_default("brandcolor_gradient_1", "#f54266");
